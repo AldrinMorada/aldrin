cf7f64d1a99e938ba2fbd06f52c0392d3aaf5e42
Aldrin Morada <moradaaldrin123@gmail.com>
2023-10-06 01:47:57 +0800

    fsdf

--- src/shared/components/Sidebar.js
+++ src/shared/components/Sidebar.js
@@ -2,48 +2,31 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 import "../css/sidebar.css";
 
-const Sidebar = ({ sideBarToggle, handleToggleSidebar }) => {
+const Sidebar = () => {
   return (
-    <div className="aside-container">
-      <aside
-        className={`${
-          sideBarToggle ? "block bg-white dark:bg-[#181a1e]" : "hidden"
-        } lg:block  fixed w-[170px]`}
-      >
-        <div className="toggle">
-          <div className="close " id="close-btn">
-            <span
-              className="material-icons-sharp hover:text-primary"
-              onClick={() => handleToggleSidebar(false)}
-            >
-              close
-            </span>
-          </div>
-        </div>
+    <aside className="lg:block fixed w-[180px] ml-8">
+      <div className="sidebar shadowed-box pt-4">
+        <NavLink to="/admin/analytics">
+          <span className="material-icons-sharp">insights</span>
+          <h3>Analytics</h3>
+        </NavLink>
 
-        <div className={` ${sideBarToggle ? "" : "shadowed-box"} sidebar pt-4`}>
-          <NavLink to="/admin/analytics">
-            <span className="material-icons-sharp">insights</span>
-            <h3>Analytics</h3>
-          </NavLink>
+        <NavLink to="/admin/users">
+          <span className="material-icons-sharp">person_outline</span>
+          <h3>Users</h3>
+        </NavLink>
 
-          <NavLink to="/admin/users">
-            <span className="material-icons-sharp">person_outline</span>
-            <h3>Users</h3>
-          </NavLink>
+        <NavLink to="/admin/trainingbonds">
+          <span className="material-icons-sharp">handshake</span>
+          <h3>Bonds</h3>
+        </NavLink>
 
-          <NavLink to="/admin/trainingbonds">
-            <span className="material-icons-sharp">handshake</span>
-            <h3>Bonds</h3>
-          </NavLink>
-
-          <NavLink to="/admin/test">
-            <span className="material-icons-sharp">report_gmailerrorred</span>
-            <h3>Test</h3>
-          </NavLink>
-        </div>
-      </aside>
-    </div>
+        <NavLink to="/admin/test">
+          <span className="material-icons-sharp">report_gmailerrorred</span>
+          <h3>Test</h3>
+        </NavLink>
+      </div>
+    </aside>
   );
 };
 
