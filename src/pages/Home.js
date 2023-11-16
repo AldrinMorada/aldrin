6dbd52ed41d5499dfb83c34db7f7402a4877c42f
Aldrin Morada <moradaaldrin123@gmail.com>
2023-11-17 05:16:48 +0800

    aldrin morada

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,20 +1,21 @@
 import React from "react";
-import Layout from "../components/SharedComponents/Layout2";
 import { Button } from "../components/SharedComponents/Button";
 const Home = () => {
   return (
-    <Layout>
-      <div className="relative">
+    <div className="-mt-10">
+      <div className="relative h-screen">
         <img
           src="https://hips.hearstapps.com/hmg-prod/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=1xw:0.74975xh;center,top&resize=1200:*"
           alt=""
-          className="max-h-[500px]"
+          className="max-h-screen"
         />
 
-        <div className="absolute inset-0 bg-black/40">
-          <div className="w-1/2 p-20">
-            <h1 className="text-white dark:text-white">Jade's Payo</h1>
-            <p className="mt-6 text-white dark:text-white leading-6">
+        <div className="absolute h-screen inset-0 bg-black/40">
+          <div className="w-1/2 px-12 py-32">
+            <span className="text-white dark:text-white text-8xl font-bold tracking-wide leading-6">
+              Jade's Payo
+            </span>
+            <p className="mt-12 text-xl tracking-wide leading-8 text-white dark:text-white">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit
               dolorum reprehenderit adipisci officia similique officiis soluta,
               consequuntur expedita fuga, placeat sequi vitae iure voluptas
@@ -26,19 +27,23 @@ const Home = () => {
               repellat provident debitis impedit obcaecati at nobis ad numquam
               cumque sint. Dolores voluptate explicabo ratione natus aliquid
               obcaecati id, possimus asperiores, voluptas, voluptatem itaque
-              excepturi odio quos adipisci! Nam
+              excepturi odio quos adipisci! Nam aque, perspiciatis laudantium
+              officia error aspernatur, fuga repellat provident debitis impedit
+              obcaecati at nobis ad numquam cumque sint. Dolores voluptate
+              explicabo ratione natus aliquid obcaecati id, possimus asperiores,
+              voluptas, voluptatem itaque excepturi odio quos adipisci! Nam
             </p>
 
             <Button
               variant={"outline"}
-              className="mt-6 w-[120px] hover:bg-primary dark:hover:bg-primary font-bold text-lg tracking-wide"
+              className="mt-20 p-8 w-[120px] hover:bg-primary dark:hover:bg-primary font-bold text-lg tracking-wide"
             >
               Notify
             </Button>
           </div>
         </div>
       </div>
-      <div className="min-h-[200px] mt-12 px-12 py-8">
+      <div className="min-h-[450px] mt-12 px-12 py-8">
         <h2>Hello Camille!</h2>
 
         <p className="mt-4">
@@ -83,39 +88,33 @@ const Home = () => {
         ></path>
       </svg>
 
-      <div className="bg-[#ff327f] min-h-[450px] text-white -mt-16 px-12 py-8">
-        <h2>Hello Camille!</h2>
+      <div className="bg-[#ff327f] min-h-[450px] grid grid-cols-2 text-white -mt-4 px-10 py-8">
+        <div></div>
+        <div className="text-center px-4">
+          <h2>Hello Camille!</h2>
 
-        <p className="font-bold text-white dark:text-white mt-4">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit
-          dolorum reprehenderit adipisci officia similique officiis soluta,
-          consequuntur expedita fuga, placeat sequi vitae iure voluptas maxime
-          facilis perspiciatis. Quidem sed dignissimos ipsum optio. Eveniet
-          deleniti excepturi, inventore nisi maxime dicta molestias, fuga ipsum
-          accusamus esse delectus! Id nihil maiores tempore? Eligendi porro
-          vitae accusantium harum, omnis totam explicabo eaque, perspiciatis
-          laudantium officia error aspernatur, fuga repellat provident debitis
-          impedit obcaecati at nobis ad numquam cumque sint. Dolores voluptate
-          explicabo ratione natus aliquid obcaecati id, possimus asperiores,
-          voluptas, voluptatem itaque excepturi odio quos adipisci! Nam vero,
-          fugit est ipsum inventore perspiciatis? Rerum nulla, neque odit in
-          amet molestias enim quo ipsum sint! Quis quod illum dolores animi?
-          Officiis autem quaerat, natus necessitatibus atque corrupti officia
-          facilis dolorum iste saepe at earum exercitationem architecto quae
-          unde alias soluta dolores. Velit nobis vitae beatae in temporibus
-          deserunt amet inventore reprehenderit eum nihil sunt praesentium
-          magnam ut, molestias veritatis reiciendis minus error quam quisquam
-          obcaecati porro necessitatibus blanditiis, natus alias. Earum aliquam
-          suscipit nostrum, vel architecto sequi nihil dolores, officia
-          doloribus vero est, laboriosam neque deleniti quidem? Deleniti
-          possimus totam fuga sed, odio veritatis ullam sequi iusto sapiente
-          incidunt, aliquid explicabo. Earum quas quam aspernatur enim quasi,
-          nihil voluptas. Atque doloribus quibusdam earum tempore, numquam
-          asperiores soluta? Pariatur natus ab optio nulla dolorem ipsum porro
-          facilis tenetur temporibus esse ex itaque deserunt, cupiditate iste!
-        </p>
+          <p className="font-bold text-white dark:text-white mt-4">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit
+            dolorum reprehenderit adipisci officia similique officiis soluta,
+            consequuntur expedita fuga, placeat sequi vitae iure voluptas maxime
+            facilis perspiciatis. Quidem sed dignissimos ipsum optio. Eveniet
+            deleniti excepturi, inventore nisi maxime dicta molestias, fuga
+            ipsum accusamus esse delectus! Id nihil maiores tempore? Eligendi
+            porro vitae accusantium harum, omnis totam explicabo eaque,
+            perspiciatis laudantium officia error aspernatur, fuga repellat
+            provident debitis impedit obcaecati at nobis ad numquam cumque sint.
+            Dolores voluptate explicabo ratione natus aliquid obcaecati id,
+            possimus asperiores, voluptas, voluptatem itaque excepturi odio quos
+            adipisci! Nam vero, fugit est ipsum inventore perspiciatis? Rerum
+            nulla, neque odit in amet molestias enim quo ipsum sint! Quis quod
+            illum dolores animi? Officiis autem quaerat, natus necessitatibus
+            atque corrupti officia facilis dolorum iste saepe at earum
+            exercitationem architecto quae unde alias soluta dolores. Velit
+            nobis vitae beatae in temporibus deserunt amet inventore
+          </p>
+        </div>
       </div>
-    </Layout>
+    </div>
   );
 };
 
